3c088fdefa9be02cc10db4ec86f669d01b53bef0
liorsh <liorsh@shenkar.ac.il>
2018-12-13 14:08:02 +0200

    upload to heroku

--- routes/shmoogle.js
+++ routes/shmoogle.js
@@ -1,23 +1,23 @@
 const express = require('express');
-const https   = require('https');
 const router  = express.Router();
 const axios   = require('axios');
 
-const SUBSCRIPTION_KEY = process.env.AZURE_SUBSCRIPTION_KEY;
+const SUBSCRIPTION_KEY = 'ed8cb9f6cda94f1eaf0b6be7e54bd9cf';
 if (!SUBSCRIPTION_KEY) {
   throw new Error('AZURE_SUBSCRIPTION_KEY is not set.')
 }
 
 function bingSearchApi(query){
+  console.log(query);
     var resultsArr = [];
-    for(var i = 0; i < 3; i++){
+    for(var i = 0; i < 100; i++){
       resultsArr.push(bingSearchQuery(query, i))
     }
     return  Promise.all(resultsArr);
 }
 
 function bingSearchQuery(query, offset){
-  console.log(encodeURIComponent(query) + '&count=10&offset=' + (offset * 10));
+  //console.log(encodeURIComponent(query) + '&count=10&offset=' + (offset * 10));
   var query = {
     url:    'https://api.cognitive.microsoft.com/bing/v7.0/search?q=',
     query:   encodeURIComponent(query) + '&count=10&offset=' + (offset * 10),
@@ -26,23 +26,23 @@ function bingSearchQuery(query, offset){
   return axios.get(query.url + query.query, {headers: query.headers})
   .then(response=>{
     var results = response.data.webPages.value;
-    //console.log(results);
     return results;
   })
-  .catch(err=>{console.log(err)})
+  .catch(err=>{//console.log(err)
+  })
      
 }
 
 
-
-
 /* GET  shmoogle query */
-router.get('/', function(req, res, next) {
-  bingSearchApi('shenkar')
+router.get('/:query', function(req, res, next) {
+  console.log(req.params.query);
+  bingSearchApi(req.params.query)
   .then((results)=>{
     res.send(results);
   })
-  .catch(err=>{console.log(err)})
+  .catch(err=>{//console.log(err)
+  })
 });
 
 
