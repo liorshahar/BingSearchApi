23a81943b318e714b70a069578212a9bcd91bb70
unknown <Administrator@It70.shenkar.ac.il>
2018-12-18 13:24:21 +0200

    Shuffle result

--- routes/shmoogle.js
+++ routes/shmoogle.js
@@ -1,23 +1,25 @@
 const express = require('express');
 const router  = express.Router();
 const axios   = require('axios');
-
+var _ = require('underscore');
 const SUBSCRIPTION_KEY = 'ed8cb9f6cda94f1eaf0b6be7e54bd9cf';
 if (!SUBSCRIPTION_KEY) {
   throw new Error('AZURE_SUBSCRIPTION_KEY is not set.')
 }
 
-function bingSearchApi(query){
+
+
+async function bingSearchApi(query){
   console.log(query);
-    var resultsArr = [];
-    for(var i = 0; i < 100; i++){
-      resultsArr.push(bingSearchQuery(query, i))
-    }
-    return  Promise.all(resultsArr);
+  var resultsArr = [];
+  for(var i = 0; i < 10 ; i++){
+    let t = await bingSearchQuery(query, i)
+    resultsArr.push(_.shuffle(t))
+  }
+  return _.shuffle(resultsArr);
 }
 
 function bingSearchQuery(query, offset){
-  //console.log(encodeURIComponent(query) + '&count=10&offset=' + (offset * 10));
   var query = {
     url:    'https://api.cognitive.microsoft.com/bing/v7.0/search?q=',
     query:   encodeURIComponent(query) + '&count=10&offset=' + (offset * 10),
@@ -28,7 +30,7 @@ function bingSearchQuery(query, offset){
     var results = response.data.webPages.value;
     return results;
   })
-  .catch(err=>{//console.log(err)
+  .catch(err=>{console.log(err)
   })
      
 }
@@ -41,7 +43,7 @@ router.get('/:query', function(req, res, next) {
   .then((results)=>{
     res.send(results);
   })
-  .catch(err=>{//console.log(err)
+  .catch(err=>{console.log(err)
   })
 });
 
