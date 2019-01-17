59ed953de9c90be580e467ec968721b47212bc89
liorsh <liorsh@shenkar.ac.il>
2019-01-17 10:27:07 +0200

    return rate to default

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ const app = express();
 const rateLimit = require("express-rate-limit");
 
 const limiter = rateLimit({
-  windowMs: 1 * 60 * 1000, // 15 minutes
-  max: 60 // limit each IP to 100 requests per windowMs
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
 });
 
 //  apply to all requests
